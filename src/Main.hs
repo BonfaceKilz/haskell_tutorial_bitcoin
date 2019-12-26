9ff14c14c9c6e43bb30c45f3e5e50af99899f1b1
BonfaceKilz <bonfacemunyoki@gmail.com>
2019-12-26 20:34:43 +0300

    Parse the json object to get the bitcoin rate

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,11 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as BS
+
 import           Network.HTTP.Simple (httpBS, getResponseBody)
+import           Control.Lens        (preview)
+import           Data.Aeson.Lens     (key, _String)
+import           Data.Text           (Text)
 
 
 fetchJSON :: IO BS.ByteString
@@ -11,7 +15,10 @@ fetchJSON = do
   res <- httpBS "https://api.coindesk.com/v1/bpi/currentprice.json"
   return (getResponseBody res)
 
+getRate :: BS.ByteString -> Maybe Text
+getRate = preview (key "bpi" . key "USD" . key "rate" . _String)
+
 main :: IO ()
 main = do
   json <- fetchJSON
-  BS.putStrLn json
+  print (getRate json)
